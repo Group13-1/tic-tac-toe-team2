1b5b8cb21b92e47380bee0b2e0537d4a958c50e1
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-04-03 01:03:18 +0800

    Update TicTacToe.java
    add some basic item

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class TicTacToe {
-
+   private static final Scanner scanner = new Scanner(System.in);
+    private static final char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
 }
