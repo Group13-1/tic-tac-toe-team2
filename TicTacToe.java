fb4f2e9995932fa4065ae2e42d2ebc1c43eb206f
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-04-03 01:04:46 +0800

    Update TicTacToe.java
    define basic method to be implemented

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,4 +5,41 @@ public class TicTacToe {
    private static final Scanner scanner = new Scanner(System.in);
     private static final char[][] board = new char[3][3];
     private static char currentPlayer = 'X';
+
+     public static void main(String[] args) {
+        return null;
+
+    }
+
+    private static void initializeBoard() {
+        return null;
+    }
+
+    private static void playGame() {
+        return null;
+    }
+
+    private static void printBoard() {
+        return null;
+    }
+
+    private static void userTurn() {
+          return null;
+    }
+
+    private static boolean isGameOver() {
+        return null;
+    }
+
+    private static boolean hasContestantWon(char player) {
+        return null;
+    }
+
+    private static boolean isBoardFull() {
+        return null;
+    }
+
+    private static void swapPlayer() {
+        return null;
+    }
 }
